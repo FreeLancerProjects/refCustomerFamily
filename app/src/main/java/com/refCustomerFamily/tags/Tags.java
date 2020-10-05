90d0c4d9ef18b7e3458a03abf687f2594d52f065
melashry <mohamedelashry323@gmail.com>
2020-10-05 14:46:21 +0200

    el3shry

--- app/src/main/java/com/refCustomerFamily/tags/Tags.java
+++ app/src/main/java/com/refCustomerFamily/tags/Tags.java
@@ -7,7 +7,6 @@ public class Tags {
     public static final String IMAGE_Places_URL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=";
     public static final String session_login = "login";
     public static final String session_logout = "logout";
-    public static final String not_tag = "V60BNS_not_tag";
     public static final int not_id = 3254;
 
 }
